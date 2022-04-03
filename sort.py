56ecd7970acb7514f4b6ad0f632e66fc2720bf31
quangngonz <46818455+quangngonz@users.noreply.github.com>
2022-04-03 22:40:24 +0700

    Fixed Sort and Counting algorithm

--- sort.py
+++ sort.py
@@ -1,5 +1,18 @@
-list_no = [10.35, 10.28, 10.05, 10.25, 10.28, 10.65, 10.47, 10.33, 10.28, 10.42]
+import tabulate
+
+list_no = [3,4,5,1,2,8,9,6,5,3,2,1,6,4,7,8,1,1,5,5,2,3,4,5,7,8,3,4,2,5,1,9,4,5,6,7,8,9,2,1,5,4,3,4,5,6,1,4,4,8]
 
 #sort the list
 list_no.sort()
-print(list_no)
+list_elements = list(set(list_no))
+
+print("List elements: ", list_elements)
+
+counts = []
+
+for element in list_elements:
+    print("Element: ", element, "Count: ", list_no.count(element))
+    counts.append([element, list_no.count(element)])
+
+# Use tabulate to draw table of counts
+print(tabulate.tabulate(counts, headers=['Element', 'Count'], tablefmt='pretty'))
