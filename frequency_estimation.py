51a80b9664f8e783f1d986ad66e13e80e4884e36
quangngonz <46818455+quangngonz@users.noreply.github.com>
2022-04-04 09:59:00 +0700

    Add Frequency Estimation

--- frequency_estimation.py
+++ frequency_estimation.py
@@ -3,10 +3,7 @@ list_of_classes = []
 for i in range(20, 25):
     list_of_classes.append(i/2)
     
-frequency = [58,
-24,
-15,
-3,]
+frequency = [58, 24, 15, 3]
 
 for class_number in range(len(frequency)):
     list_of_classes[class_number] = list_of_classes[class_number] + 0.25
